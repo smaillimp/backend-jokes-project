0bfa58e7c9375dfba81aa97abc982f43464ccd42
smaillimp <sp2020.ing@gmail.com>
2021-12-09 14:34:02 +0100

    updated cors

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', '<deployed URL>'],
+    origin: ['http://localhost:3000', 'https://greatest-dev-jokes.netlify.app'],
   })
 );
 
